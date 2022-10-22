062858504a4fe71ef7dca78cdc2ccbe06c02f5fe
Atanas809 <atanas2380@abv.bg>
2022-10-22 10:01:05 +0300

    Added file for gui app

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from render_screen import *
+
+if __name__ == "__main__":
+    tk = Tk()
+    tk.geometry("600x600")
+    tk.title("My GUI Shop")
+
+    main_screen(tk)
+    tk.mainloop()
